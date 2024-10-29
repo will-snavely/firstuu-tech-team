d74d95a2bacb88725063ac1eb13348be49f19e79
First Unitarian <firstunitarian@Firsts-MacBook-Pro.local>
2024-10-29 10:53:08 -0400

    format

--- google_apps/signup_tools/CalendarSync.js
+++ google_apps/signup_tools/CalendarSync.js
@@ -7,8 +7,8 @@ function syncCalendarOnTimer() {
 
 function syncCalendarOnEdit(e) {
   var range = e.range;
-  if(e.range.getRow() === 1) {
-    if(e.range.getLastRow() === 1) {
+  if (e.range.getRow() === 1) {
+    if (e.range.getLastRow() === 1) {
       return;
     } else {
       range = range.offset(1, 0, range.getNumRows() - 1);
@@ -29,30 +29,30 @@ function syncCalendar(range) {
 
   var data = sheet.getSheetValues(startRow, 1, lastRow, 10);
   var earliest = new Date();
-  earliest.setFullYear(3000) 
+  earliest.setFullYear(3000)
   var latest = new Date();
   latest.setFullYear(1000);
   for (var i = 0; i < data.length; i++) {
     const entry = data[i];
-    if(String(entry[0]).trim() !== '') {
-      if(entry[0].getTime() < earliest.getTime()) {
+    if (String(entry[0]).trim() !== '') {
+      if (entry[0].getTime() < earliest.getTime()) {
         earliest = new Date(entry[0]);
       }
-      if(entry[0].getTime() > latest.getTime()) {
+      if (entry[0].getTime() > latest.getTime()) {
         latest = new Date(entry[0]);
       }
-    } 
+    }
   }
 
   console.log("Earliest Date: " + earliest);
   console.log("Latest Date: " + latest);
   earliest.setDate(earliest.getDate() - 5);
-  earliest.setHours(12,0,0,0);
+  earliest.setHours(12, 0, 0, 0);
   latest.setDate(latest.getDate() + 1);
-  latest.setHours(12,0,0,0);
+  latest.setHours(12, 0, 0, 0);
   var events = calendar.getEvents(earliest, latest);
   const eventMap = new Map();
-  for(var i = 0; i < events.length; i++) {
+  for (var i = 0; i < events.length; i++) {
     eventMap.set(events[i].getId(), events[i]);
   }
 
@@ -62,9 +62,9 @@ function syncCalendar(range) {
     var endTime = data[i][2];
     var description = data[i][7];
 
-    if(String(date).trim() === '' 
-      || String(startTime).trim() === '' 
-      || String(endTime).trim() === '' 
+    if (String(date).trim() === ''
+      || String(startTime).trim() === ''
+      || String(endTime).trim() === ''
       || String(description).trim() === '') {
       continue;
     }
@@ -74,30 +74,30 @@ function syncCalendar(range) {
 
     var crew = [];
     for (var j = 0; j < 4; j++) {
-      var signup = data[i][3+j];
-      if(String(signup).trim() !== '') {
+      var signup = data[i][3 + j];
+      if (String(signup).trim() !== '') {
         crew.push(signup);
       }
     }
 
     var crewString = "(Volunteers Needed)"
-    if(crew.length > 0) {
+    if (crew.length > 0) {
       crewString = "(" + crew.join(", ") + ")";
     }
 
     const eventDesc = description + " " + crewString;
     const eventId = data[i][8];
 
-    if(String(eventId) === '') {
+    if (String(eventId) === '') {
       var event = calendar.createEvent(eventDesc, startTime, endTime);
       console.log("Created Event with ID: " + event.getId());
       event.setColor(CalendarApp.EventColor.BLUE);
       sheet.getRange(startRow + i, 9).setValue(event.getId());
     } else {
       var event = eventMap.get(eventId);
-      if(event.getTitle() !== eventDesc 
-          || event.getStartTime().getTime() !== startTime.getTime() 
-          || event.getEndTime().getTime() !== endTime.getTime()) {
+      if (event.getTitle() !== eventDesc
+        || event.getStartTime().getTime() !== startTime.getTime()
+        || event.getEndTime().getTime() !== endTime.getTime()) {
         console.log("Updating Event with ID: " + event.getId());
         event.setTitle(eventDesc);
         event.setTime(startTime, endTime);
